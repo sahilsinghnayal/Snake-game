d0e8f0298767449bb757699cd8e53942ef900739
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-03-16 15:50:00 +0530

    first

---
+++ script.js
@@ -0,0 +1,5 @@
+let direction={x:0 , y:0};
+const foodsound=new Audio('food.mp3');
+const gameover= new Audio('gameover.mp3');
+const move=new Audio('move.mp3');
+const music=new Audio('music.mp3');
